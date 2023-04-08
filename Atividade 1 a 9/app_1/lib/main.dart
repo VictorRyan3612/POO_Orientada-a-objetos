741ec7fac4cdfae94a61035466ee64bb912d6322
Victor <117222760+VictorRyan36122@users.noreply.github.com>
2023-04-08 16:10:59 -0300

    App 0.0.1

--- Atividade 1 a 9/app_1/lib/main.dart
+++ Atividade 1 a 9/app_1/lib/main.dart
@@ -2,12 +2,16 @@ import 'package:flutter/material.dart';
 
 void main() {
   MaterialApp app = MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-      ),
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
 
+      home: Scaffold(
 
-      home: Text("Apenas começando..."));
+        appBar: AppBar(title: Text("Meu app")),
 
+        body: Text("Apenas começando..."),
+
+        bottomNavigationBar: Text("Botão 1"),
+
+      ));
   runApp(app);
 }
