b281bdd95e41800b493a671008a222bb80e3f1b4
Victor <117222760+VictorRyan36122@users.noreply.github.com>
2023-04-08 17:43:01 -0300

    App 0.1.1

--- Atividade 1 a 9/app_1/lib/main.dart
+++ Atividade 1 a 9/app_1/lib/main.dart
@@ -2,63 +2,54 @@ import 'package:flutter/material.dart';
 
 void main() {
   MaterialApp app = MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.deepPurple,
-      colorScheme: ColorScheme.dark()
-      ),
+    theme: ThemeData(primarySwatch: Colors.deepPurple,
+    colorScheme: const ColorScheme.dark()
+    ),
 
 
-      home: Scaffold(
-        appBar: AppBar(title: Text("Lista de coisas para fazer")),
+    home: Scaffold(
+      appBar: AppBar(title: const Text("Lista de coisas para fazer")),
 
-        body: Center(
-          child: Column(children: [
-              Text("Estilzar o texto",
-              style: TextStyle(fontWeight: FontWeight.bold)),
+      body: Center(
+        child: Column(
+          children: const [
+            Text("Estilzar o texto",
+            style: TextStyle(fontWeight: FontWeight.bold)),
 
-              const Text("Mudar a cor",
-              style: TextStyle(color: Color.fromARGB(255, 192, 0, 9))
-              ),
+            Text("Mudar a cor",
+            style: TextStyle(color: Color.fromARGB(255, 192, 0, 9))
+            ),
 
-              Text("Aumentar a fonte",
-              style: TextStyle(fontSize: 50),
-              ),
+            Text("Aumentar a fonte",
+            style: TextStyle(fontSize: 50),
+            ),
 
-              Text("Mudar ícone do aplicativo")
-            ],
-          )
-
-
-        ),
-
-        bottomNavigationBar: BottomNavigationBar(items: const [
-          BottomNavigationBarItem(
-            label: 'Voltar',
-            icon: Icon(Icons.arrow_back),
-          ),
-
-          BottomNavigationBarItem(
-            label: "Inicio", 
-            icon: Icon(Icons.home)),
-
-          BottomNavigationBarItem(
-            label: "Avançar", 
-            icon: Icon(Icons.arrow_forward)),
-          ]
-        
-        
-        
+            Text("Mudar ícone do aplicativo")
+          ],
         )
 
+      ),
 
-          // child: Row(
-            
-          //   children: [
-          //     Text("Voltar"),
-          //     Text("Tela inicial"),
-          //     Text("Avançar")
-          //   ],
-          // ),)
+      bottomNavigationBar: BottomNavigationBar(items: const [
+        BottomNavigationBarItem(
+          label: 'Voltar',
+          icon: Icon(Icons.arrow_back),
+        ),
 
-      ));
+        BottomNavigationBarItem(
+          label: "Inicio", 
+          icon: Icon(Icons.home)),
+
+        BottomNavigationBarItem(
+          label: "Avançar", 
+          icon: Icon(Icons.arrow_forward)),
+        ]
+      
+      
+      
+      )
+
+    )
+  );
   runApp(app);
 }
