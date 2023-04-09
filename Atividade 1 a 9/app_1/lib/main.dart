f489946135f7082327f57fa04d23f4f3cc8c83c3
Victor <117222760+VictorRyan36122@users.noreply.github.com>
2023-04-09 13:08:01 -0300

    App 0.1.4

--- Atividade 1 a 9/app_1/lib/main.dart
+++ Atividade 1 a 9/app_1/lib/main.dart
@@ -15,49 +15,7 @@ class Myapp extends StatelessWidget{
       ),
 
 
-      home: Scaffold(
-        appBar: AppBar(title: const Text("Lista de coisas para fazer")),
-
-        body: Center(
-          child: Column(
-            children: const [
-              Text("Estilzar o texto",
-              style: TextStyle(fontWeight: FontWeight.bold)),
-
-              Text("Mudar a cor",
-              style: TextStyle(color: Color.fromARGB(255, 192, 0, 9))
-              ),
-
-              Text("Aumentar a fonte",
-              style: TextStyle(fontSize: 50),
-              ),
-
-              Text("Mudar ícone do aplicativo")
-            ],
-          )
-
-        ),
-
-        bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(
-              label: 'Voltar',
-              icon: Icon(Icons.arrow_back),
-            ),
-
-            BottomNavigationBarItem(
-              label: "Inicio", 
-              icon: Icon(Icons.home)),
-
-            BottomNavigationBarItem(
-              label: "Avançar", 
-              icon: Icon(Icons.arrow_forward)),
-          ]
-        
-        )
-
-
-      )
+      home: HomePage()
 
     );
 
@@ -78,7 +36,50 @@ class HomePage extends StatefulWidget{
 class HomePageStates extends State<HomePage>{
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return
+    Scaffold(
+      appBar: AppBar(title: const Text("Lista de coisas para fazer")),
+
+      body: Center(
+        child: Column(
+          children: const [
+            Text("Estilzar o texto",
+            style: TextStyle(fontWeight: FontWeight.bold)),
+
+            Text("Mudar a cor",
+            style: TextStyle(color: Color.fromARGB(255, 192, 0, 9))
+            ),
+
+            Text("Aumentar a fonte",
+            style: TextStyle(fontSize: 50),
+            ),
+
+            Text("Mudar ícone do aplicativo")
+          ],
+        )
+
+      ),
+
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            label: 'Voltar',
+            icon: Icon(Icons.arrow_back),
+          ),
+
+          BottomNavigationBarItem(
+            label: "Inicio", 
+            icon: Icon(Icons.home)),
+
+          BottomNavigationBarItem(
+            label: "Avançar", 
+            icon: Icon(Icons.arrow_forward)),
+        ]
+      
+      )
+
+
+    );
   }
 
 }
