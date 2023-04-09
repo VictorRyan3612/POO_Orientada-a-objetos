80c5681a3a7029f6776bf3e3b3dd54d6dd4b3597
Victor <117222760+VictorRyan36122@users.noreply.github.com>
2023-04-09 12:19:35 -0300

    App 0.1.2

--- Atividade 1 a 9/app_1/lib/main.dart
+++ Atividade 1 a 9/app_1/lib/main.dart
@@ -1,55 +1,66 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  MaterialApp app = MaterialApp(
-    theme: ThemeData(primarySwatch: Colors.deepPurple,
-    colorScheme: const ColorScheme.dark()
-    ),
+  runApp(Myapp());
+}
 
+class Myapp extends StatelessWidget{
 
-    home: Scaffold(
-      appBar: AppBar(title: const Text("Lista de coisas para fazer")),
-
-      body: Center(
-        child: Column(
-          children: const [
-            Text("Estilzar o texto",
-            style: TextStyle(fontWeight: FontWeight.bold)),
-
-            Text("Mudar a cor",
-            style: TextStyle(color: Color.fromARGB(255, 192, 0, 9))
-            ),
-
-            Text("Aumentar a fonte",
-            style: TextStyle(fontSize: 50),
-            ),
-
-            Text("Mudar ícone do aplicativo")
-          ],
-        )
-
+ @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      theme: ThemeData(
+      primarySwatch: Colors.deepPurple,
+      colorScheme: const ColorScheme.dark()
       ),
 
-      bottomNavigationBar: BottomNavigationBar(items: const [
-        BottomNavigationBarItem(
-          label: 'Voltar',
-          icon: Icon(Icons.arrow_back),
+
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Lista de coisas para fazer")),
+
+        body: Center(
+          child: Column(
+            children: const [
+              Text("Estilzar o texto",
+              style: TextStyle(fontWeight: FontWeight.bold)),
+
+              Text("Mudar a cor",
+              style: TextStyle(color: Color.fromARGB(255, 192, 0, 9))
+              ),
+
+              Text("Aumentar a fonte",
+              style: TextStyle(fontSize: 50),
+              ),
+
+              Text("Mudar ícone do aplicativo")
+            ],
+          )
+
         ),
 
-        BottomNavigationBarItem(
-          label: "Inicio", 
-          icon: Icon(Icons.home)),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              label: 'Voltar',
+              icon: Icon(Icons.arrow_back),
+            ),
 
-        BottomNavigationBarItem(
-          label: "Avançar", 
-          icon: Icon(Icons.arrow_forward)),
-        ]
-      
-      
-      
+            BottomNavigationBarItem(
+              label: "Inicio", 
+              icon: Icon(Icons.home)),
+
+            BottomNavigationBarItem(
+              label: "Avançar", 
+              icon: Icon(Icons.arrow_forward)),
+          ]
+        
+        )
+
+
       )
+      
+    );
 
-    )
-  );
-  runApp(app);
+  }
+
 }
