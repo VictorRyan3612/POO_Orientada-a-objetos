915dbcf779b946d88041217aa23ac04cc764812e
Victor <117222760+VictorRyan36122@users.noreply.github.com>
2023-04-11 20:17:18 -0300

    App 0.1.7.2 Receita 2 Excercise 1

--- Atividade 1 a 9/app_1/lib/home_page.dart
+++ Atividade 1 a 9/app_1/lib/home_page.dart
@@ -17,7 +17,7 @@ class HomePage extends StatefulWidget{
 
 
 class HomePageStates extends State<HomePage>{
-  final Color _currentTextColor = Color.fromARGB(255, 16, 106, 180);
+
 
 
   @override
@@ -66,44 +66,7 @@ class HomePageStates extends State<HomePage>{
       ),  // Populate the Drawer in the next step.
 
 
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: NetworkImage(
-              "https://cf.shopee.com.br/file/d2343b578c8d343870438a4046bc9091",
-            ),
-            fit: BoxFit.cover,
-          ),
-        ),
-      
-        
-       child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Expanded(
-              child: Align(
-                alignment: Alignment.topCenter,
-                child: Column(
-                  
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Estilzar o texto",
-                    style: TextStyle(fontWeight: FontWeight.bold, color: _currentTextColor)
-                    ),
-
-                    Text("Mudar a cor",
-                    style: TextStyle(color: _currentTextColor)
-                    ),
-                    Text("Aumentar a fonte",
-                    style: TextStyle(fontSize: 50, color: _currentTextColor)
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
+      body: MyBody(),
 
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
@@ -158,3 +121,59 @@ class MyAppBar extends StatelessWidget{
 
     }
   }
+
+
+
+
+
+
+
+
+class MyBody extends StatelessWidget{
+  MyBody();
+
+  final Color _currentTextColor = Color.fromARGB(255, 16, 106, 180);
+
+  @override
+  Widget build(BuildContext context){
+    return
+      Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage(
+              "https://cf.shopee.com.br/file/d2343b578c8d343870438a4046bc9091",
+            ),
+            fit: BoxFit.cover,
+          ),
+        ),
+      
+        
+       child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: Column(
+                  
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Estilzar o texto",
+                    style: TextStyle(fontWeight: FontWeight.bold, color: _currentTextColor)
+                    ),
+
+                    Text("Mudar a cor",
+                    style: TextStyle(color: _currentTextColor)
+                    ),
+                    Text("Aumentar a fonte",
+                    style: TextStyle(fontSize: 50, color: _currentTextColor)
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+}
