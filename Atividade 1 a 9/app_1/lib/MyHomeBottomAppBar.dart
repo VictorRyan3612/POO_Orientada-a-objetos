f560ebe8f9b0a78c4864c0852ae90fff10925963
Victor <117222760+VictorRyan36122@users.noreply.github.com>
2023-04-11 21:55:12 -0300

    App 0.1.7.6 Receita 2 Exercise 4

--- Atividade 1 a 9/app_1/lib/MyHomeBottomAppBar.dart
+++ Atividade 1 a 9/app_1/lib/MyHomeBottomAppBar.dart
@@ -1,38 +1,43 @@
 import 'package:flutter/material.dart';
 
 
+// Adaptado do chatGPT
+class NewNavBar extends StatelessWidget {
+  final List<IconData> MyIcones;
 
-class MyHomeBottomAppBar extends StatelessWidget{
-  MyHomeBottomAppBar();
+  NewNavBar({required this.MyIcones});
 
   void botaoTocado(int index) {
     print("Tocaram no botão $index");
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      onTap: botaoTocado,
+      items: MyIcones.map((icon) {
+        return BottomNavigationBarItem(
+          icon: Icon(icon),
+          label: '', // rótulo vazio para não mostrar o texto
+        );
+      }
+      ).toList()
+    );
+  }
+}
+
+class MyHomeBottomAppBar extends StatelessWidget {
+  MyHomeBottomAppBar();
 
 
   @override
-  Widget build(BuildContext context){
-  
-      return BottomNavigationBar(onTap: botaoTocado,
-        items: const [
-          BottomNavigationBarItem(
-            label: "Voltar",
-            icon: Icon(Icons.arrow_back),
-          ),
-
-          BottomNavigationBarItem(
-            label: "Tela inicial", 
-            icon: Icon(Icons.home)
-            ),
-
-          BottomNavigationBarItem(
-            label: "Avançar", 
-            icon: Icon(Icons.arrow_forward)
-          )
-
-        ]
-      );
-
-    }
+  Widget build(BuildContext context) {
+    return NewNavBar(
+      MyIcones: [
+        Icons.arrow_back,
+        Icons.home,
+        Icons.arrow_forward,
+      ],
+    );
   }
+}
