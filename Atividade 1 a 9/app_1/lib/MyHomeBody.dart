5cdd2334c5d101f11a6439af90d95939d0862a5c
Victor <117222760+VictorRyan36122@users.noreply.github.com>
2023-04-16 15:14:34 -0300

    Receita 3 Part 1

--- Atividade 1 a 9/app_1/lib/MyHomeBody.dart
+++ Atividade 1 a 9/app_1/lib/MyHomeBody.dart
@@ -3,50 +3,38 @@ import 'package:flutter/material.dart';
 
 
 class MyHomeBody extends StatelessWidget{
-  MyHomeBody();
+  List<String> objects;
+
+  MyHomeBody( {this.objects = const [] });
+
 
   final Color _currentTextColor = Color.fromARGB(255, 16, 106, 180);
 
   @override
   Widget build(BuildContext context){
-    return
-      Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: NetworkImage(
-              "https://cf.shopee.com.br/file/d2343b578c8d343870438a4046bc9091",
-            ),
-            fit: BoxFit.cover,
-          ),
-        ),
-      
-        
-       child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Expanded(
-              child: Align(
-                alignment: Alignment.topCenter,
-                child: Column(
-                  
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Estilzar o texto",
-                        style: TextStyle(fontWeight: FontWeight.bold, color: _currentTextColor)
-                    ),
+     List<Expanded> allTheLines = [];
 
-                    Text("Mudar a cor",
-                        style: TextStyle(color: _currentTextColor)
-                    ),
-                    Text("Aumentar a fonte",
-                        style: TextStyle(fontSize: 50, color: _currentTextColor)
-                    ),
-                  ],
-                ),
+      for (var obj in objects){
+        allTheLines.add( 
+          Expanded(                
+            child: Center(child: Text(obj)),
+          )
+        );
+      } 
+      return 
+        Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage(
+                "https://cf.shopee.com.br/file/d2343b578c8d343870438a4046bc9091",
               ),
+              fit: BoxFit.cover,
             ),
-          ],
-        ),
-      );
+          ),
+        
+        
+        );
+    
+        Column(children: allTheLines);
     }
 }
