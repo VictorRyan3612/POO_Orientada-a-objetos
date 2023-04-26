52232e53716f864654074810b759a5e8e8008e06
Victor <117222760+VictorRyan36122@users.noreply.github.com>
2023-04-25 21:44:11 -0300

    Receita 1 Exercise 10 e 11 base

---
+++ un1_receita1_10e11/README.md
@@ -0,0 +1,3 @@
+# un1_receita1_10e11
+
+A new Flutter project.
---
+++ un1_receita1_10e11/android/app/src/main/kotlin/com/example/un1_receita1_10e11/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.un1_receita1_10e11
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
