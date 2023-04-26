43c5e9e4e150aa71eab141530344dcfab851cd01
Victor <117222760+VictorRyan36122@users.noreply.github.com>
2023-04-25 21:18:35 -0300

    Receita 1 base

---
+++ un1_receita1/README.md
@@ -0,0 +1,3 @@
+# un1_receita1
+
+A new Flutter project.
---
+++ un1_receita1/android/app/src/main/kotlin/com/example/un1_receita1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.un1_receita1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
