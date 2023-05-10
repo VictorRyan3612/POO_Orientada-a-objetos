44afec9e14acce02f3ef76f461e0235ba7d6bb00
Victor <117222760+VictorRyan36122@users.noreply.github.com>
2023-05-10 02:26:54 -0300

    Receita 6 part 2.1

--- app_notas/lib/var_global.dart
+++ app_notas/lib/var_global.dart
@@ -13,41 +13,45 @@ class DataService{
   }
 
   void carregarCervejas(){
-  tableStateNotifier.value = [
-    {
-      "name": "La Fin Du Monde",
-      "style": "Bock",
-      "ibu": "65"
-    },
-    {
-      "name": "Sapporo Premiume",
-      "style": "Sour Ale",
-      "ibu": "54"
-    },
-    {
-      "name": "Duvel", 
-      "style": "Pilsner", 
-      "ibu": "82"
-    }
-  ];
+    tableStateNotifier.value = [
+      {
+        "name": "La Fin Du Monde", 
+        "style": "Bock", 
+        "ibu": "65"
+      },
+      {
+        "name": "Sapporo Premiume", 
+        "style": "Sour Ale", 
+        "ibu": "54"
+      },
+      {
+        "name": "Duvel",
+        "style": "Pilsner",
+        "ibu": "82"
+      },
+
+      {
+        "name": "La Fin Du Monde", 
+        "style": "Bock", 
+        "ibu": "65"
+      },
+      {
+        "name": "Sapporo Premiume", 
+        "style": "Sour Ale", 
+        "ibu": "54"
+      },
+
+      {
+        "name": "Duvel",
+        "style": "Pilsner",
+        "ibu": "82"
+      },
+    
+    
+    ];
   }
 }
 
-
-// var dataObjects = [
-//   {"name": "La Fin Du Monde", "style": "Bock", "ibu": "65"},
-//   {"name": "Sapporo Premiume", "style": "Sour Ale", "ibu": "54"},
-//   {"name": "Duvel", "style": "Pilsner", "ibu": "82"},
-//   {"name": "La Fin Du Monde", "style": "Bock", "ibu": "65"},
-//   {"name": "Sapporo Premiume", "style": "Sour Ale", "ibu": "54"},
-//   {"name": "Duvel", "style": "Pilsner", "ibu": "82"},
-//   {"name": "La Fin Du Monde", "style": "Bock", "ibu": "65"},
-//   {"name": "Sapporo Premiume", "style": "Sour Ale", "ibu": "54"},
-//   {"name": "Duvel", "style": "Pilsner", "ibu": "82"},
-//   {"name": "La Fin Du Monde", "style": "Bock", "ibu": "65"},
-//   {"name": "Sapporo Premiume", "style": "Sour Ale", "ibu": "54"},
-//   {"name": "Duvel", "style": "Pilsner", "ibu": "82"},
-// ];
 
 const iconesnomesVar = [
   "Voltar",
