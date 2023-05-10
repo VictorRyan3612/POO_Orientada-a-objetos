f2b7eca52b58c362ef4076721a965131ba31bfdc
Victor <117222760+VictorRyan36122@users.noreply.github.com>
2023-05-10 02:14:22 -0300

    Receita 6 Part 1.5.3

--- app_notas/lib/home_page.dart
+++ app_notas/lib/home_page.dart
@@ -114,36 +114,4 @@ class HomePageStates extends State<HomePage>{
 
     ));
   }
-
-}
-void carregarCervejas(){
-  tableStateNotifier.value = [{
-          "name": "La Fin Du Monde",
-          "style": "Bock",
-          "ibu": "65"
-
-          },
-
-          {
-
-          "name": "Sapporo Premiume",
-
-          "style": "Sour Ale",
-
-          "ibu": "54"
-
-          },
-
-          {
-
-          "name": "Duvel", 
-
-          "style": "Pilsner", 
-
-          "ibu": "82"
-
-          }
-
-        ];
-
 }
