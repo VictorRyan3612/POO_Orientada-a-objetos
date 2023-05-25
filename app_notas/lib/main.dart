bb90ffb0320615ad85a67ee97ab0578f9bb94a6a
Victor <117222760+VictorRyan36122@users.noreply.github.com>
2023-05-25 00:34:17 -0300

    Tema v1.0

--- app_notas/lib/main.dart
+++ app_notas/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'home_page.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -20,69 +22,4 @@ class MyApp extends HookWidget {
       );
     
   }
-}
-
-class HomePageApp extends StatelessWidget {
-  final ValueNotifier<Brightness> _currentBrightness;
-
-  HomePageApp(this._currentBrightness);
-  void _toggleTheme() {
-    _currentBrightness.value = _currentBrightness.value == Brightness.light
-        ? Brightness.dark
-        : Brightness.light;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: MyAppBar(_currentBrightness),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: _toggleTheme,
-          child: Text(
-            'Alternar Tema',
-            style: TextStyle(fontSize: 20),
-          ),
-        ),
-      ),
-    );
-  }
-
-}
-
-
-
-class MyAppBar extends StatelessWidget implements PreferredSizeWidget{
-  final ValueNotifier<Brightness> _currentBrightness;
-
-  MyAppBar(this._currentBrightness);
-
-  @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
-
-
-  @override
-  Widget build(BuildContext context) {
-    return AppBar(
-      title: Text('Mudança de Tema'),
-      actions: [
-        PopupMenuButton<Brightness>(
-          onSelected: (Brightness brightness) {
-            _currentBrightness.value = brightness;
-          },
-          itemBuilder: (BuildContext context) => [
-            PopupMenuItem<Brightness>(
-              value: Brightness.light,
-              child: Text('Tema Claro'),
-            ),
-            PopupMenuItem<Brightness>(
-              value: Brightness.dark,
-              child: Text('Tema Escuro'),
-            ),
-          ],
-        ),
-      ],
-    );
-  }
-
 }
--- app_notas/lib/my_home_appbar.dart
+++ app_notas/lib/my_home_appbar.dart
@@ -1,50 +1,40 @@
 import 'package:flutter/material.dart';
 
 class MyHomeAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const MyHomeAppBar({super.key});
+  final ValueNotifier<Brightness> _currentBrightness;
+
+  const MyHomeAppBar(this._currentBrightness, {super.key});
 
   @override
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
       title: const Text(
         "Lista de coisas para fazer",
-        style: TextStyle(fontSize: 25)),
+        style: TextStyle(fontSize: 25)
+      ),
         
       actions: [
-        // adaptado de https://www.fluttercampus.com/guide/223/popup-menu-on-flutter-appbar/
-        PopupMenuButton(
+        PopupMenuButton<Brightness>(
           icon: const Icon(Icons.more_vert),
-          itemBuilder: (context) {
-            return const [
-              PopupMenuItem<int>(
-                value: 0,
-                child: Text("Tema Branco"),
-              ),
-              PopupMenuItem<int>(
-                value: 1,
-                child: Text("Tema Preto"),
-              ),
-              PopupMenuItem<int>(
-                value: 2,
-                child: Text("Configurações"),
-              ),
-            ];
+          onSelected: (Brightness brightness) {
+            _currentBrightness.value = brightness;
           },
-        onSelected: (value) {
-          if (value == 0) {
-            print("Tema Branco selecionado.");
-          } 
-          else if (value == 1) {
-            print("Tema Preto selecionado.");
-          } 
-          else if (value == 2) {
-            print("Configurações selecionado.");
-          }
-        }),
-      ],
+          itemBuilder: (BuildContext context) => [
+            PopupMenuItem<Brightness>(
+              value: Brightness.light,
+              child: Text('Tema Claro'),
+            ),
+            PopupMenuItem<Brightness>(
+              value: Brightness.dark,
+              child: Text('Tema Escuro'),
+            ),
+          ],
+        ),
+      ]
     );
   }
 }
