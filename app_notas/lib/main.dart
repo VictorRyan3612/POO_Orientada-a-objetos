599287d74d32c405d64f112f40eb2370eb60398a
Victor <117222760+VictorRyan36122@users.noreply.github.com>
2023-05-24 23:14:23 -0300

    Temas v0.2

--- app_notas/lib/main.dart
+++ app_notas/lib/main.dart
@@ -1,46 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 void main() {
-  // MyApp app = const MyApp();
-
   runApp(Temas());
 }
 
-
-
-class Temas extends StatefulWidget {
-  @override
-  _TemasState createState() => _TemasState();
-}
-
-class _TemasState extends State<Temas> {
-  Brightness _currentBrightness = Brightness.light;
-
-  void _toggleTheme() {
-    setState(() {
-      _currentBrightness = _currentBrightness == Brightness.light
-          ? Brightness.dark
-          : Brightness.light;
-    });
-  }
-
+class Temas extends HookWidget {
   @override
   Widget build(BuildContext context) {
+    final _currentBrightness = useState(Brightness.dark);
+
+    void _toggleTheme() {
+      _currentBrightness.value = _currentBrightness.value == Brightness.light
+          ? Brightness.dark
+          : Brightness.light;
+    }
+
     return MaterialApp(
       title: 'Mudança de Tema',
       theme: ThemeData(
-        brightness: _currentBrightness,
+        brightness: _currentBrightness.value,
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
         appBar: AppBar(
           title: Text('Mudança de Tema'),
-          
         ),
         body: Center(
           child: ElevatedButton(
             onPressed: _toggleTheme,
-          child: Text(
+            child: Text(
               'Alternar Tema',
               style: TextStyle(fontSize: 20),
             ),
