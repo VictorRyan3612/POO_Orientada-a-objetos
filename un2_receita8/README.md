fd322e03f954d5a6eb45c2a7649b7acfc7730b4e
Victor <117222760+VictorRyan36122@users.noreply.github.com>
2023-05-26 13:01:19 -0300

    Receita 8 part 0

---
+++ un2_receita8/README.md
@@ -0,0 +1,3 @@
+# un2_receita8
+
+A new Flutter project.
---
+++ un2_receita8/android/app/src/main/kotlin/com/example/un2_receita8/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.un2_receita8
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
