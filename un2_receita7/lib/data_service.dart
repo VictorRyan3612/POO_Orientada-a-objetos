16a00d389a91f8d1298dacd8954d5228ebf4fea2
Victor <117222760+VictorRyan36122@users.noreply.github.com>
2023-05-26 12:58:48 -0300

    Mudança de nome pasta receita7 part 3

---
+++ un2_receita7/lib/data_service.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+
+var estadoAplicativo = {
+  "objects": [],
+  "props": [],
+  "columnsNames": [
+    "Propriedade 1", 
+    "Propriedade 2", 
+    "Propriedade 3"
+  ]
+};
+
+class DataService{
+  final ValueNotifier<Map<String,dynamic>> tableStateNotifier= new ValueNotifier(estadoAplicativo);
+  int querySize = 5;
+
+
+  void carregar(index){
+    var res = null;
+    print('carregar #1 - antes de carregarCervejas');
+    if (index == 0) res = carregarCafes();
+    if (index == 1) res = carregarCervejas();
+    print('carregar #2 - carregarCervejas retornou $res');
+    if (index == 2) res = carregarNacoes();
+
+  }
+
+  void setQuerySize(int newSize) {
+    querySize = newSize;
+  }
+
+
+  Future<void> carregarCafes() async{
+    var cafesUri = Uri(
+      scheme: "https",
+      host: "random-data-api.com",
+      path: "api/coffee/random_coffee",
+      queryParameters: {'size': '$querySize'}
+    );
+
+    var jsonString = await http.read(cafesUri);
+    var cafesJson = jsonDecode(jsonString);
+    
+    tableStateNotifier.value = {
+      "objects": cafesJson,
+      "props": [
+        "blend_name",
+        "origin",
+        "variety"
+      ],
+      "columnsNames": [
+        "Nomes",
+        "Origem",
+        "Variedade"
+      ]
+    };
+  }
+
+
+  
+
+
+  Future<void> carregarCervejas() async{
+
+    var beersUri = Uri(
+      scheme: 'https',
+      host: 'random-data-api.com',
+      path: 'api/beer/random_beer',
+      queryParameters: {'size': '$querySize'}
+    );
+
+    print('carregarCervejas #1 - antes do await');
+    var jsonString = await http.read(beersUri);
+
+    print('carregarCervejas #2 - depois do await');
+    var beersJson = jsonDecode(jsonString);
+
+
+
+    tableStateNotifier.value = {
+      "objects": beersJson,
+      "props": [
+        "name",
+        "style",
+        "ibu"
+      ],
+      "columnsNames": [
+        "Nomes",
+        "Estilo",
+        "IBU"
+      ]
+    };
+
+  }
+    Future<void> carregarNacoes() async{
+
+    var nacoesUri = Uri(
+      scheme: 'https',
+      host: 'random-data-api.com',
+      path: 'api/nation/random_nation',
+      queryParameters: {'size': '$querySize'}
+    );
+
+
+    var jsonString = await http.read(nacoesUri);
+    var nacoesJson = jsonDecode(jsonString);
+
+
+
+    tableStateNotifier.value = {
+      "objects": nacoesJson,
+      "props": [
+        "nationality",
+        "language",
+        "capital"
+      ],
+      "columnsNames": [
+        "Nacionalidade",
+        "Linguagem",
+        "Capital"
+      ]
+    };
+  }
+
+}
