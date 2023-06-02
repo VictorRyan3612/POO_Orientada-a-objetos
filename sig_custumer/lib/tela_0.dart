9ccf2a41a0a70de66ea408a41f849a9c0eeb6c00
Victor <117222760+VictorRyan36122@users.noreply.github.com>
2023-06-02 19:28:52 -0300

    Tela 0 v0.0.4

--- sig_custumer/lib/tela_0.dart
+++ sig_custumer/lib/tela_0.dart
@@ -1,1 +1,34 @@
 import 'package:flutter/material.dart';
+
+class Tela0 extends StatelessWidget {
+  const Tela0({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text('Tela 0'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.orange,
+              ),
+              child: Text('Ir para a tela 1'),
+              onPressed: () {},
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.teal,
+              ),
+              child: Text('Ir para a tela 2'),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
