d26ea7743afdf5ab61aaba73610f177fff9bd2b1
Victor <117222760+VictorRyan36122@users.noreply.github.com>
2023-06-02 19:19:23 -0300

    Tela 1 v.0.0.2

--- sig_custumer/lib/tela_1.dart
+++ sig_custumer/lib/tela_1.dart
@@ -1,1 +1,21 @@
 import 'package:flutter/material.dart';
+
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {},
+          child: const Text('ir para tela 2'),
+        ),
+      ),
+    );
+  }
+}
