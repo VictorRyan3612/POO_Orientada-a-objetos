95dd944ad6696a4854f79fb773613ae30e983c96
Victor <117222760+VictorRyan36122@users.noreply.github.com>
2023-06-02 19:24:03 -0300

    Tela 2 v0.0.3

--- sig_custumer/lib/tela_2.dart
+++ sig_custumer/lib/tela_2.dart
@@ -1,1 +1,22 @@
 import 'package:flutter/material.dart';
+
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+          },
+          child: const Text('Voltar!'),
+        ),
+      ),
+    );
+  }
+}
