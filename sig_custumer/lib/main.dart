bf3b8801578838d5f3969fc69985a80d32098a35
Victor <117222760+VictorRyan36122@users.noreply.github.com>
2023-06-02 20:18:33 -0300

    Hook v.0.0.0

--- sig_custumer/lib/main.dart
+++ sig_custumer/lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
 import 'package:sig_custumer/modulo_midias.dart';
 import 'package:sig_custumer/tela_menu.dart';
 import 'package:sig_custumer/modulo_usuario.dart';
 import 'package:sig_custumer/modulo_redes.dart';
 
+
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends HookWidget {
   const MainApp({super.key});
 
   @override
