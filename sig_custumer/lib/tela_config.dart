a7920e0a4001e9e944c0c609a2054c34a06bc3d9
Victor <117222760+VictorRyan36122@users.noreply.github.com>
2023-06-03 03:03:11 -0300

    CardSettingsSwitch Temas v2.0.0

--- sig_custumer/lib/tela_config.dart
+++ sig_custumer/lib/tela_config.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-// import 'package:settings_ui/settings_ui.dart';
 import 'package:card_settings/card_settings.dart';
 
 class TelaConfigs extends HookWidget implements PreferredSizeWidget{
@@ -39,10 +38,14 @@ class TelaConfigs extends HookWidget implements PreferredSizeWidget{
                         trueLabel: '', 
                         falseLabel: '',
                         label: 'Modo noturno',
-                        initialValue: false,
-                        
-                        onSaved: (value) {
-                          // Salvar configurações
+                        initialValue: true,
+                        onChanged: (value) {
+                          if (currentBrightness.value == Brightness.dark) {
+                            currentBrightness.value = Brightness.light;
+                          } 
+                          else {
+                            currentBrightness.value = Brightness.dark;
+                          }
                         },
                       ),
                     ],
