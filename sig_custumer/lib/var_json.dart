5488203ad3e674bb220602c14e6189c9170689ac
Victor <117222760+VictorRyan36122@users.noreply.github.com>
2023-06-02 22:03:16 -0300

    Tela menu 0.1.1

---
+++ sig_custumer/lib/var_json.dart
@@ -0,0 +1,19 @@
+final List<Map<String, dynamic>> menuItens = [
+  {
+    'title': 'Todo List',
+    'count': '2 Items',
+  },
+  {
+    'title': 'Notes',
+    'count': '12 Items',
+  },
+  {
+    'title': 'Agenda',
+    'count': '4 Items',
+  },
+  {
+    'title': 'Settings',
+    'count': '6 Items',
+  },
+  
+];
