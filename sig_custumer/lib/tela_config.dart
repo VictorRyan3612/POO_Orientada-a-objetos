664cb01426c747e7d1b9dc7d89059978855d0a51
Victor <117222760+VictorRyan36122@users.noreply.github.com>
2023-06-02 21:50:19 -0300

    Mudança de temas v0.1.0

--- sig_custumer/lib/tela_config.dart
+++ sig_custumer/lib/tela_config.dart
@@ -2,17 +2,43 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 
-class TelaConfigs extends HookWidget {
-  final ValueNotifier<Brightness>? currentBrightness;
+class TelaConfigs extends HookWidget implements PreferredSizeWidget{
+  final ValueNotifier<Brightness> currentBrightness;
 
   const TelaConfigs({required this.currentBrightness, super.key});
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Tela de configurações'),
+
+      actions: [
+        PopupMenuButton<Brightness>(
+          icon: const Icon(Icons.more_vert),
+          onSelected: (Brightness brightness) {
+            currentBrightness.value = brightness;
+          },
+          itemBuilder: (BuildContext context) => [
+            const PopupMenuItem<Brightness>(
+              value: Brightness.light,
+              child: Text('Tema Claro'),
+            ),
+            const PopupMenuItem<Brightness>(
+              value: Brightness.dark,
+              child: Text('Tema Escuro'),
+            ),
+          ],
+        ),
+      ]
+
       ),
+
+
       body: Center(
         child: ElevatedButton(
           onPressed: () {
