a0ddfa4f06e9b4f985ae9807f7e376444d04e5af
Victor <vitorsilva3612@gmail.com>
2023-06-16 19:09:02 -0300

    Receita 8 parte 1

---
+++ all_receitas/lib/receita8/datatable.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class DataTableWidget extends StatelessWidget {
+  
+  // final List jsonObjects;
+  List<dynamic> objects;
+  final List columnNames;
+  final List propertyNames;
+
+  DataTableWidget({super.key, 
+    this.objects = const [],
+    this.columnNames = const [], 
+    this.propertyNames= const []}
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return DataTable(
+      columns: columnNames.map( 
+        (name) => DataColumn(
+          label: Expanded(
+            child: Text(name, style: const TextStyle(fontWeight: FontWeight.bold))
+          )
+        )
+      ).toList(),
+
+
+      rows: objects.map( 
+        (obj) => DataRow(
+            cells: propertyNames.map(
+              (propName) => DataCell(Text(obj[propName]))
+            ).toList()
+          )
+        ).toList()
+    );
+  }
+}
