61d55fabb85a8fd33df54803636e5d0fbcee9874
Victor <vitorsilva3612@gmail.com>
2023-06-28 14:16:51 -0300

    Organização de arquivos parte 2

---
+++ un3_receita_9e10/lib/view/widget.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+import '../data/data_service.dart';
+import '../view/form.dart';
+import '../view/datatable.dart';
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        colorScheme: const ColorScheme.dark(),
+      ),
+      
+      debugShowCheckedModeBanner:false,
+
+      home: Scaffold(
+        appBar: AppBar( 
+          title: const Text("Dicas"),
+          ),
+
+        body: Column(
+          children: [
+            Align(
+              alignment: Alignment.center,
+              child: Container(
+                constraints: const BoxConstraints(maxWidth: 600),
+                child: MyCustomForm(callback: dataService.setQuerySize),
+              ),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child:
+                  ValueListenableBuilder(
+                  valueListenable: dataService.tableStateNotifier,
+                  builder: (_, value, __) {
+                    switch (value['status']){
+                        case TableStatus.idle: 
+                          return const Padding (
+                              padding: EdgeInsets.all(30.0),
+                              child: Text("Toque algum botão")
+                            );
+                        case TableStatus.loading:
+                          return const Padding (
+                              padding: EdgeInsets.all(200.0),
+                              child: CircularProgressIndicator()
+                            );
+                        case TableStatus.ready: 
+                          return Center(
+                            child: DataTableWidget(
+                              objects: value["dataObjects"],
+                              propertyNames: value["props"],
+                              columnNames: value["columnsNames"],
+                            ),
+                          );
+                        case TableStatus.error: 
+                          return const Padding (
+                              padding: EdgeInsets.all(30.0),
+                              child: Text("Erro de conexão: Verifique sua conexão com a internet.")
+                            );
+                      }
+                      return const Text("Erro desconhecido"); 
+                  }
+              ),
+                
+              ),
+            ),
+          ],
+        ),
+
+        
+        
+        bottomNavigationBar: NewNavBar(itemSelectedCallback: dataService.carregar),
+      ));
+  }
+
+}
+
+
+
+
+
+class NewNavBar extends HookWidget {
+  
+  final _itemSelectedCallback;
+
+  NewNavBar({super.key, itemSelectedCallback}):
+    _itemSelectedCallback = itemSelectedCallback ?? (int);
+
+
+  @override
+  Widget build(BuildContext context) {
+    
+    var state = useState(1);
+
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      onTap: (index){
+        state.value = index;
+        _itemSelectedCallback(index);                
+      }, 
+
+      currentIndex: state.value,
+
+
+      items: const [
+        BottomNavigationBarItem(
+          label: "Cafés",
+          icon: Icon(Icons.coffee_outlined),
+        ),
+
+        BottomNavigationBarItem(
+          label: "Cervejas", 
+          icon: Icon(Icons.local_drink_outlined)
+        ),
+
+        BottomNavigationBarItem(
+          label: "Nações", 
+          icon: Icon(Icons.flag_outlined)
+        ),
+        BottomNavigationBarItem(
+          label: "Tipos Sanguineos", 
+          icon: Icon(Icons.bloodtype)
+        )
+      ]
+
+
+    );
+  }
+
+}
+
+
