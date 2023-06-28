9a4a223d864ed6593b9d8f04a1754e1e0232cda3
Victor <vitorsilva3612@gmail.com>
2023-06-28 14:12:04 -0300

    Organização de arquivos parte 1

--- un3_receita_9e10/lib/main.dart
+++ un3_receita_9e10/lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'data_service.dart';
-import 'form.dart';
-import 'datatable.dart';
+import 'data/data_service.dart';
+import 'view/form.dart';
+import 'view/datatable.dart';
 
 
 final dataService = DataService();
