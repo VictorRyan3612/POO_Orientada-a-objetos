632a4711039ee4cbf245f18f83e7d330d9204b1f
Victor <vitorsilva3612@gmail.com>
2023-07-16 19:55:20 -0300

    Receita 10d passo 1

---
+++ un3_receita_9e10/lib/data/models.dart
@@ -0,0 +1,24 @@
+class Coffee{
+  String blendName, origin, variety;
+  Coffee({required this.blendName, required this.origin, required this.variety});
+}
+
+class Beer{
+  String name, style, ibu;
+  Beer({required this.name, required this.style,required this.ibu});
+}
+
+class Nation{
+  String nationality, language, capital;
+  Nation({required this.nationality, required this.capital, required this.language});
+}
+
+class Blood{
+  String type, rh_factor, group;
+  Blood({required this.type, required this.rh_factor, required this.group});
+}
+
+class Device{
+  String manufacturer, model, platform;
+  Device({required this.manufacturer, required this.model, required this.platform});
+}
